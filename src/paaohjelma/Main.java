ccdb346f38ced713744e3da7c7f1b6b122284c8e
Lauri Ali-Hokka <laurialihokka@gmail.com>
2020-01-16 19:34:10 +0200

    main luokassa tällä hetkellä topiclist testausta

--- src/paaohjelma/Main.java
+++ src/paaohjelma/Main.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        /*
         Scanner lukija = new Scanner(System.in);
         Writer kirjoittaja = new Writer();
         Reader reader = new Reader();
@@ -18,7 +19,33 @@ public class Main {
         }catch (IOException e){
             System.out.println("Jotain tapahtui");
             e.printStackTrace();
-        }
+        }*/
+
+        Topiclist aihelistaus = new Topiclist();
+        aihelistaus.addTopicsToList("Java");
+        aihelistaus.addTopicsToList("Dava");
+        aihelistaus.addTopicsToList("Hava");
+
+
+
+        System.out.println(aihelistaus.getTopic("Java").getTitle());
+        System.out.println(aihelistaus.getTopic("Java").getId());
+        System.out.println(aihelistaus.getTopic("Java").getId());
+
+        System.out.println(aihelistaus.getTopic("Dava").getTitle());
+        System.out.println(aihelistaus.getTopic("Dava").getId());
+
+        System.out.println(aihelistaus.getTopic("Hava").getTitle());
+        System.out.println(aihelistaus.getTopic("Hava").getId());
+        System.out.println(aihelistaus.getTopic("Hava").getId());
+        System.out.println(aihelistaus.getTopic("Hava").isComplete());
+        System.out.println(aihelistaus.getTopic("Hava").isComplete());
+        System.out.println(aihelistaus.getTopic("Java"));
+
+        aihelistaus.getAllTopics();
+
+
+
 
         }
     }
