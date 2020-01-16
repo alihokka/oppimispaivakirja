28466a1e92f1822d3586b111c94f48d7bf4502df
Lauri Ali-Hokka <laurialihokka@gmail.com>
2020-01-16 21:18:10 +0200

    roskakoodia poistettu Writerista

--- src/paaohjelma/Writer.java
+++ src/paaohjelma/Writer.java
@@ -4,27 +4,14 @@ import java.io.*;
 
 public class Writer {
     Topiclist topiclist = new Topiclist();
-    public Writer(){
 
+    public Writer() {
     }
-
-    /*
-
-    public void writeToFile(String filename) throws IOException {
-        String s = new BufferedReader(new InputStreamReader(System.in)).readLine();
-        FileWriter fw = new FileWriter(filename, true); // Write to end of file
-        PrintWriter pw = new PrintWriter(fw);
-        pw.println(s);
-        pw.close();
-        fw.close();
-    }
-
-     */
 
     BufferedWriter writer = null;
 
 
-    public void kirjoitaTiedostoon(String stringtowrite){
+    public void kirjoitaTiedostoon(String stringtowrite) {
         try {
             writer = new BufferedWriter(new FileWriter("topics.txt", true));
             topiclist.addTopicsToList(stringtowrite);
