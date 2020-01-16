663a3775b3779e3ef1cc4093c2ec040c02720b52
Lauri Ali-Hokka <laurialihokka@gmail.com>
2020-01-16 16:50:33 +0200

    Interfacepohja luotu

--- src/paaohjelma/Interface.java
+++ src/paaohjelma/Interface.java
@@ -1,20 +1,58 @@
 package paaohjelma;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Interface {
-    private Topic topic;
     private Reader reader;
     private Writer writer;
     private Scanner scanner;
 
-        public Interface(Topic topic, Reader reader, Writer writer, Scanner scanner){
-            this.topic = topic;
+        public Interface(Reader reader, Writer writer, Scanner scanner){
             this.reader = reader;
             this.writer = writer;
             this.scanner = scanner;
         }
-        public void start(){
+
+    public void kaynnista() throws IOException {
+
+
+
+        while(true) {
+            System.out.println("---------------------------------------");
+            System.out.println("Komennot:");
+            System.out.println("listaa = listaa tämän kansion tiedostot");
+            System.out.println("kirjoita = kirjoita tiedostoon");
+            System.out.println("avaa = näytä tiedoston sisältö");
+            System.out.println("---------------------------------------");
+
+            String valinta = scanner.nextLine();
+
+            if (valinta.equals("listaa")) {
+                System.out.println("moi");
+            }
+
+            if (valinta.equals("kirjoita")) {
+                System.out.print("Mihin tiedostoon kirjoitetaan?: ");
+                String kirjoitusvalinta = scanner.nextLine();
+                System.out.println("Lisää tekstiä:");
+                writer.writeToFile(kirjoitusvalinta);
+                continue;
+            }
+
+            if(valinta.equals("avaa")){
+                System.out.println("Mikä tiedosto avataan?");
+                String avattava = scanner.nextLine();
+
+                reader.readFile(avattava);
+            }
+
+            if(valinta.isEmpty()){
+                break;
+            }
 
         }
+
+
+    }
 }
