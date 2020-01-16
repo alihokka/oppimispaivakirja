98727bc1cefeb9ed4a43e3687b4a154c866040f5
Lauri Ali-Hokka <laurialihokka@gmail.com>
2020-01-16 16:18:39 +0200

    uniikki id korjattu, gettereitä lisätty

--- src/paaohjelma/Topic.java
+++ src/paaohjelma/Topic.java
@@ -3,12 +3,51 @@ package paaohjelma;
 import java.time.LocalDate;
 
 public class Topic {
-    private int id;
+    static int id = 0;
     private String title;
     private String description;
     private String additionalSource;
     private boolean complete;
     private LocalDate creationDate;
     private LocalDate completionDate;
+
+    public Topic(String title){
+        this.title = title;
+        this.creationDate = LocalDate.now();
+        this.complete = false;
+        this.id = id+1;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+
+    public String getAdditionalSource(){
+        return this.additionalSource;
+    }
+
+    public boolean isComplete(){
+        return this.complete;
+    }
+
+    public String getCreationDate(){
+        return creationDate.toString();
+    }
+
+    public String getCompletionDate(){
+        if(completionDate == null){
+            return "Ei valmis.";
+        }else{
+            return this.completionDate.toString();
+        }
+    }
 
 }
