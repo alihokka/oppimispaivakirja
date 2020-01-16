81a2fbe6327b002bc919679f79665a17f245de7d
Lauri Ali-Hokka <laurialihokka@gmail.com>
2020-01-16 16:18:02 +0200

    Mainissa testailua

--- src/paaohjelma/Main.java
+++ src/paaohjelma/Main.java
@@ -1,8 +1,25 @@
 package paaohjelma;
 
+
 public class Main {
 
     public static void main(String[] args) {
+        Topic aihe = new Topic("Java");
+
+        System.out.println(aihe.getId());
+        System.out.println(aihe.getTitle());
+        System.out.println(aihe.getCreationDate());
+        System.out.println(aihe.getDescription());
+        System.out.println(aihe.getAdditionalSource());
+        System.out.println(aihe.getCompletionDate());
+        System.out.println(aihe.isComplete());
+
+        Topic toinenaihe = new Topic("Eri juttu");
+
+        System.out.println(toinenaihe.getId());
+        System.out.println(toinenaihe.getTitle());
+        System.out.println(toinenaihe.getCreationDate());
+        System.out.println(toinenaihe.getCompletionDate());
 
     }
 }
